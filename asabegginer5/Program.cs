b52c8b602339a6fb53cd13bab39b884064ec8b88
iampr <iampr@PRABIN>
2023-01-03 17:38:12 +0545

    Add project files.

---
+++ asabegginer5/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int number = 10;
+        bool Isnumber10 = number == 10 ? true : false;
+        Console.WriteLine("Number == 10 is {0}", Isnumber10);
+    }
+}
+ 
